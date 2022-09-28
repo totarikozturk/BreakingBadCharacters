cfb8c185aa4a382c3fe0ca2752e9bdf377d3244a
Tarik <tarkztrk@icloud.com>
2022-09-28 11:30:50 +0300

    first commit

---
+++ BreakingBadCharacters/BreakingBadCharacters/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BreakingBadCharacters
+//
+//  Created by TarıkOzturk on 28.09.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication,
+                     configurationForConnecting connectingSceneSession: UISceneSession,
+                     options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+
+        UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+    }
+}
---
+++ BreakingBadCharacters/BreakingBadCharacters/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BreakingBadCharacters
+//
+//  Created by TarıkOzturk on 28.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
