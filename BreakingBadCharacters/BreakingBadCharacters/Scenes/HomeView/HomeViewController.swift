1be810194a510299dc68691447374510835138bc
Tarik <tarkztrk@icloud.com>
2022-09-28 11:51:56 +0300

    Make folder

--- BreakingBadCharacters/BreakingBadCharacters/Scenes/HomeView/HomeViewController.swift
+++ BreakingBadCharacters/BreakingBadCharacters/Scenes/HomeView/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  BreakingBadCharacters
 //
 //  Created by TarıkOzturk on 28.09.2022.
@@ -7,10 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
 }
---
+++ BreakingBadCharacters/BreakingBadCharacters/Scenes/SplashView/SplashViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  BreakingBadCharacters
+//
+//  Created by TarıkOzturk on 28.09.2022.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
